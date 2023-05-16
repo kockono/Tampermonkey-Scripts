7ca5abae198c674bcd147dff12197da2e1fae0b6
Chris Cobos <55373948+kockono@users.noreply.github.com>
2023-05-16 15:21:39 -0600

    Create AproveRequest.js
    Aprove pull request from Github

---
+++ AproveRequest.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Aprove
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.addEventListener('keydown', function(event) {
+
+        // Shorcut Alt + l
+        if (event.altlKey && event.key === 'a') {
+
+            setTimeout(function() {
+                // Guardar boton aprove
+                const aproveButton = document.querySelector('.btn.btn-primary.btn-sm.ml-2');
+                aproveButton.click();
+
+            }, 250); // 250 es el tiempo en milisegundos (0.25 segundos)
+
+            setTimeout(function() {
+                // Guardar boton aprove
+                const reviewButton = document.querySelectorAll('.js-review-changes');
+                reviewButton.click();
+
+            }, 500); // 500 es el tiempo en milisegundos (0.5 segundos)
+
+            setTimeout(function() {
+            const radioInput = document.querySelector('input[name="pull_request_review[event]"][value="approve"]');
+                  radioInput.click();
+            }, 750); // 750 es el tiempo en milisegundos (0.75 segundos)
+
+           setTimeout(function() {
+               const spanElement = document.querySelector('span.Button-label');
+               spanElement.click();
+            }, 1000); // 1000 es el tiempo en milisegundos (1.0 segundos)
+
+
+          } // If event click ctrl + l
+    });
+    // Your code here...
+})();
