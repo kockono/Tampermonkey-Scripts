4df21eae769b4ec5015c20f452f8cc46cf1bc6e0
Chris Cobos <55373948+kockono@users.noreply.github.com>
2023-06-20 10:44:13 -0600

    Update AproveRequestGitHub.js

--- AproveRequestGitHub.js
+++ AproveRequestGitHub.js
@@ -7,6 +7,7 @@
 // @match        https://github.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
 
 (function() {
@@ -15,23 +16,23 @@
 
         // Shorcut Ctrl + a
         if (event.ctrlKey && event.key === 'a') {
-            // setTimeout(function() {
+             setTimeout(function() {
                 // Guardar boton aprove
-              //  const aproveButton = document.querySelector('.btn.btn-primary.btn-sm.ml-2');
-              //  aproveButton.click();
+                const aproveButton = document.querySelector('a.btn.btn-primary.btn-sm.ml-2');
+                aproveButton.click();
 
-            // }, 250); // 250 es el tiempo en milisegundos (0.25 segundos)
+             }, 250); // 250 es el tiempo en milisegundos (0.25 segundos)
 
             setTimeout(function() {
                 // Guardar boton aprove
                 const reviewButton = document.querySelector('.js-review-changes');
                 reviewButton.click();
-            }, 500); // 500 es el tiempo en milisegundos (0.5 segundos)
+            }, 1000); // 1000 es el tiempo en milisegundos (1.0 segundo)
 
             setTimeout(function() {
             const radioInput = document.querySelector('input[name="pull_request_review[event]"][value="approve"]');
                   radioInput.click();
-            }, 750); // 750 es el tiempo en milisegundos (0.75 segundos)
+            }, 1250); // 1250 es el tiempo en milisegundos (1.25 segundos)
 
            setTimeout(function() {
                const spanElements = document.querySelectorAll('span.Button-label');
@@ -40,7 +41,7 @@
                     spanElements[i].click();
                    }
                }
-            }, 1000); // 1000 es el tiempo en milisegundos (1.0 segundos)
+            }, 1500); // 1500 es el tiempo en milisegundos (1.0 segundos)
 
 
           } // If event click ctrl + l
